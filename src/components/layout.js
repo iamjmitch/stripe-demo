cabcf386500c7af0fc44e29d6e0d8316c9bbfcc3
James Mitchell <iamjmitch1990@gmail.com>
2021-02-19 23:25:07 +1000

    Initial commit from gatsby: (https://github.com/iamjmitch/gatsby-starter.git)

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styled from "styled-components"
+
+const StyledLayout = styled.div`
+  color: red;
+`
+
+const Layout = () => {
+  return <StyledLayout>Hello There</StyledLayout>
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+//--dependancies--
+import React from "react"
+import styled from "styled-components"
+
+//--components--
+import Layout from "../components/layout"
+
+const IndexPage = () => {
+  return <Layout>Hello there</Layout>
+}
+
+export default IndexPage
